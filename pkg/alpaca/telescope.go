c39d8e20c1c0b893b09752088389bf743bc29e51
Michael Roberts <michael@observerly.com>
2022-02-03 17:18:37 +0000

    chore: Updated DocString for GetAlignmentMode() func.

--- pkg/alpaca/telescope.go
+++ pkg/alpaca/telescope.go
@@ -46,9 +46,10 @@ func NewTelescope(clientId uint32, secure bool, domain string, ip string, port i
 /*
 	GetAlignmentMode()
 
-	Returns the alignment mode of the mount (Alt/Az, Polar, German Polar).
+	@returns the alignment mode of the mount (Alt/Az, Polar, German Polar).
 	The alignment mode is specified as an integer value from the
 	AlignmentModes Enum.
+	@see https://ascom-standards.org/api/#/Telescope%20Specific%20Methods/get_telescope__device_number__alignmentmode
 */
 func (t *Telescope) GetAlignmentMode() (AlignmentMode, error) {
 	mode, err := t.Alpaca.GetInt32Response("telescope", t.DeviceNumber, "alignmentmode")
