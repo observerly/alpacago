ef41a01d44dd6a212313f4fbe09ee8c324f73434
Michael Roberts <michael@observerly.com>
2022-02-03 15:22:17 +0000

    feat: Added NewTelescope() to alpaca module.
    feat: Added NewTelescope() function for init of the telescope device with the underlying ASCOMAlpacaAPIClient. Includes associated test suite for module export definition and expected output.

---
+++ pkg/alpaca/telescope_test.go
@@ -0,0 +1,69 @@
+package alpaca
+
+import "testing"
+
+func TestNewTelescopeBaseURL(t *testing.T) {
+	telescope := NewTelescope(65535, false, "", "0.0.0.0", 8000, 0, 1)
+
+	var got string = telescope.Alpaca.UrlBase
+	var want string = "http://0.0.0.0:8000"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewTelescopeBaseURLForHost(t *testing.T) {
+	telescope := NewTelescope(65535, true, "virtserver.swaggerhub.com/ASCOMInitiative", "", -1, 0, 1)
+
+	var got string = telescope.Alpaca.UrlBase
+	var want string = "https://virtserver.swaggerhub.com/ASCOMInitiative"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewTelescopeClientID(t *testing.T) {
+	telescope := NewTelescope(65535, false, "", "0.0.0.0", 8000, 0, 1)
+
+	var got uint32 = telescope.Alpaca.ClientId
+	var want uint32 = 65535
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewTelescopeTransactionID(t *testing.T) {
+	telescope := NewTelescope(65535, false, "", "0.0.0.0", 8000, 0, 1)
+
+	var got uint32 = telescope.Alpaca.TransactionId
+	var want uint32 = 0
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewTelescopeDeviceNumber(t *testing.T) {
+	telescope := NewTelescope(65535, false, "", "0.0.0.0", 8000, 0, 1)
+
+	var got uint = telescope.DeviceNumber
+	var want uint = 0
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewTelescopeTrackingMode(t *testing.T) {
+	telescope := NewTelescope(65535, false, "", "0.0.0.0", 8000, 0, 1)
+
+	var got = telescope.Tracking
+	var want = Alt_Az
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
