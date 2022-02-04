ccb7f57be60eca9803adafd01d269ce1619579cb
Michael Roberts <michael@observerly.com>
2022-02-04 11:57:04 +0000

    feat: Added IsAtPark() to alpaca module.
    feat: Added IsAtPark() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpaca/telescope_test.go
+++ pkg/alpaca/telescope_test.go
@@ -170,3 +170,23 @@ func TestNewTelescopeAtHome(t *testing.T) {
 		t.Errorf("got %q, wanted %t", telescope.Alpaca.ErrorMessage, want)
 	}
 }
+
+func TestNewTelescopeAtPark(t *testing.T) {
+	telescope := NewTelescope(65535, true, "virtserver.swaggerhub.com/ASCOMInitiative", "", -1, 0, 1)
+
+	var got, err = telescope.IsAtPark()
+
+	var want bool = true
+
+	if err != nil {
+		t.Errorf("got %q, wanted %t", err, want)
+	}
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	if telescope.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q, wanted %t", telescope.Alpaca.ErrorMessage, want)
+	}
+}
