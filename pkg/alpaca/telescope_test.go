5aa93f7b400572709fac6b6048ed2dacc6c06559
Michael Roberts <michael@observerly.com>
2022-02-08 16:40:06 +0000

    feat: Added (t *Telescope) GetFocalLength() to alpaca module.
    feat: Added GetFocalLength() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpaca/telescope_test.go
+++ pkg/alpaca/telescope_test.go
@@ -666,3 +666,25 @@ func TestNewTelescopeEquatorialSystem(t *testing.T) {
 		t.Errorf("got %q, wanted %d", telescope.Alpaca.ErrorMessage, want)
 	}
 }
+
+func TestNewTelescopeFocalLength(t *testing.T) {
+	time.Sleep(delay * time.Second)
+
+	telescope := NewTelescope(65535, true, "virtserver.swaggerhub.com/ASCOMInitiative", "", -1, 0, 1)
+
+	var got, err = telescope.GetFocalLength()
+
+	var want float64 = 1.1
+
+	if err != nil {
+		t.Errorf("got %q, wanted %f", err, want)
+	}
+
+	if math.Abs(got-want) > 0.00001 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+
+	if telescope.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q, wanted %f", telescope.Alpaca.ErrorMessage, want)
+	}
+}
