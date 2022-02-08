8421cac03cd446ef52b7fcb5c1099ad7cd0e301a
Michael Roberts <michael@observerly.com>
2022-02-08 15:50:45 +0000

    feat: Added (t *Telescope) DoesRefraction() to alpaca module.
    feat: Added DoesRefraction() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpaca/telescope_test.go
+++ pkg/alpaca/telescope_test.go
@@ -623,3 +623,25 @@ func TestNewTelescopeDeclinationRate(t *testing.T) {
 		t.Errorf("got %q, wanted %f", telescope.Alpaca.ErrorMessage, want)
 	}
 }
+
+func TestNewTelescopeDoesRefraction(t *testing.T) {
+	time.Sleep(delay * time.Second)
+
+	telescope := NewTelescope(65535, true, "virtserver.swaggerhub.com/ASCOMInitiative", "", -1, 0, 1)
+
+	var got, err = telescope.DoesRefraction()
+
+	var want bool = true
+
+	if err != nil {
+		t.Errorf("got %q, wanted %t", err, want)
+	}
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+
+	if telescope.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q, wanted %t", telescope.Alpaca.ErrorMessage, want)
+	}
+}
