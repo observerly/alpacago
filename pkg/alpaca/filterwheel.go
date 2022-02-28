c14fef025aa201df1d40f9cc13354deb75ef1578
Michael Roberts <michael@observerly.com>
2022-02-28 14:13:49 +0000

    feat: Added GetNames() to alpaca module.
    feat: Added GetNames() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpaca/filterwheel.go
+++ pkg/alpaca/filterwheel.go
@@ -25,3 +25,13 @@ func NewFilterWheel(clientId uint32, secure bool, domain string, ip string, port
 func (f *FilterWheel) GetFocusOffsets() ([]uint32, error) {
 	return f.Alpaca.GetUInt32ListResponse("filterwheel", f.DeviceNumber, "focusoffsets")
 }
+
+/*
+	GetNames()
+
+	@returns the names of the filters.
+	@see https://ascom-standards.org/api/#/FilterWheel%20Specific%20Methods/get_filterwheel__device_number__names
+*/
+func (f *FilterWheel) GetNames() ([]string, error) {
+	return f.Alpaca.GetStringListResponse("filterwheel", f.DeviceNumber, "names")
+}
