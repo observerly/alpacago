82e14c2adb2e37bc930af65dbc1ebe41bb095346
Michael Roberts <michael@observerly.com>
2022-02-28 14:05:22 +0000

    feat: Added GetFocusOffsets() to alpaca module.
    feat: Added GetFocusOffsets() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpaca/filterwheel_test.go
+++ pkg/alpaca/filterwheel_test.go
@@ -1,6 +1,8 @@
 package alpaca
 
-import "testing"
+import (
+	"testing"
+)
 
 var filterwheel = NewFilterWheel(65535, true, "alpaca.observerly.com", "", -1, 0)
 
@@ -54,5 +56,43 @@ func TestNewFilterWheelDeviceNumber(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewFilterWheelGetFocusOffsets(t *testing.T) {
+	var got, err = filterwheel.GetFocusOffsets()
+
+	want := [6]uint32{8280, 8079, 9234, 790, 6553, 4650}
+
+	if err != nil {
+		t.Errorf("got %q, wanted %q", err, want)
+	}
+
+	if got[0] != want[0] {
+		t.Errorf("got %d, wanted %q", got[0], want[0])
+	}
+
+	if got[1] != want[1] {
+		t.Errorf("got %d, wanted %q", got[1], want[1])
+	}
+
+	if got[2] != want[2] {
+		t.Errorf("got %d, wanted %q", got[2], want[2])
+	}
+
+	if got[3] != want[3] {
+		t.Errorf("got %d, wanted %q", got[3], want[3])
+	}
+
+	if got[4] != want[4] {
+		t.Errorf("got %d, wanted %q", got[4], want[4])
+	}
+
+	if got[5] != want[5] {
+		t.Errorf("got %d, wanted %q", got[5], want[5])
+	}
+
+	if telescope.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q, wanted %d", telescope.Alpaca.ErrorMessage, want)
 	}
 }
