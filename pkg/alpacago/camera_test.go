5d7570c8f5235a354dbdd41b8d5896ad4731c287
Michael J. Roberts <michael@observerly.com>
2022-04-04 18:26:44 +0100

    feat: Added NewCamera() function to alpaca module.
    feat: Added NewCamera() function for init of the camera module with the underlying ASCOMAlpacaAPIClient. Includes associated test suite for module export definition and expected output.

---
+++ pkg/alpacago/camera_test.go
@@ -0,0 +1,58 @@
+package alpacago
+
+import "testing"
+
+var camera = NewCamera(65535, true, "alpaca.observerly.com", "", -1, 0)
+
+func TestNewCameraBaseURL(t *testing.T) {
+	camera := NewCamera(65535, false, "", "0.0.0.0", 8000, 0)
+
+	var got string = camera.Alpaca.UrlBase
+	var want string = "http://0.0.0.0:8000"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewCameraBaseURLForHost(t *testing.T) {
+	var got string = camera.Alpaca.UrlBase
+	var want string = "https://alpaca.observerly.com"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewCameraClientID(t *testing.T) {
+	camera := NewCamera(65535, false, "", "0.0.0.0", 8000, 0)
+
+	var got uint32 = camera.Alpaca.ClientId
+	var want uint32 = 65535
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewCameraTransactionID(t *testing.T) {
+	camera := NewCamera(65535, false, "", "0.0.0.0", 8000, 0)
+
+	var got uint32 = camera.Alpaca.TransactionId
+	var want uint32 = 0
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestNewCameraDeviceNumber(t *testing.T) {
+	camera := NewCamera(65535, false, "", "0.0.0.0", 8000, 0)
+
+	var got uint = camera.DeviceNumber
+	var want uint = 0
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
