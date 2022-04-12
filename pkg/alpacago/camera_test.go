0961b701b798584d539dbf4adac0b897106ff465
Michael J. Roberts <michael@observerly.com>
2022-04-12 14:01:09 +0100

    feat: Added GetExposureResolution() to alpaca module.
    feat: Added GetExposureResolution() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpacago/camera_test.go
+++ pkg/alpacago/camera_test.go
@@ -506,3 +506,21 @@ func TestNewCameraGetExposureMin(t *testing.T) {
 		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
 	}
 }
+
+func TestNewCameraGetExposureResolution(t *testing.T) {
+	camera.SetConnected(true)
+
+	var got, err = camera.GetExposureResolution()
+
+	if err != nil {
+		t.Errorf("got %q", err)
+	}
+
+	if got < 0 && got > 1 {
+		t.Errorf("got %v, but expected the exposure resolution value to be a realistic value", got)
+	}
+
+	if camera.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
+	}
+}
