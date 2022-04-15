05cd092cf835ac33a22170954a9aeff73be00eb3
Michael J. Roberts <michael@observerly.com>
2022-04-15 11:50:49 +0100

    feat: Added GetLastExposureDuration() to alpaca module.
    feat: Added GetLastExposureDuration() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpacago/camera_test.go
+++ pkg/alpacago/camera_test.go
@@ -738,3 +738,21 @@ func TestNewCameraIsPulseGuiding(t *testing.T) {
 		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
 	}
 }
+
+func TestNewCameraGetLastExposureDuration(t *testing.T) {
+	camera.SetConnected(true)
+
+	var got, err = camera.GetLastExposureDuration()
+
+	if err != nil {
+		t.Errorf("got %q", err)
+	}
+
+	if got < 0 && got > 1 {
+		t.Errorf("got %v, but expected exposure duration value to be a realistic value", got)
+	}
+
+	if camera.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
+	}
+}
