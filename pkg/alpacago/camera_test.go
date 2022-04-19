e862c75212a17eb7c3c251b851b381b485602032
Michael J. Roberts <michael@observerly.com>
2022-04-19 11:56:21 +0100

    feat: Added GetStartY() to alpaca module.
    feat: Added GetStartY() to alpaca module. Includes associated test suite for module export definition and expected output.

--- pkg/alpacago/camera_test.go
+++ pkg/alpacago/camera_test.go
@@ -1014,3 +1014,21 @@ func TestNewCameraGetStartX(t *testing.T) {
 		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
 	}
 }
+
+func TestNewCameraGetStartY(t *testing.T) {
+	camera.SetConnected(true)
+
+	var got, err = camera.GetStartY()
+
+	if err != nil {
+		t.Errorf("got %q", err)
+	}
+
+	if got < 0 && got > 10000 {
+		t.Errorf("got %v, but expected the subframe start position for the Y axis value to be a realistic value", got)
+	}
+
+	if camera.Alpaca.ErrorNumber != 0 {
+		t.Errorf("got %q", camera.Alpaca.ErrorMessage)
+	}
+}
